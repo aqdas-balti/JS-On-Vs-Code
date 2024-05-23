e48702d1a12481e5555ec7d1ecf4daddcd5bd345
Aqdas Ali <aqdasali584@gmail.com>
2024-05-23 13:34:18 +0500

    displayed output

--- 01-Basics/01_variables.js
+++ 01-Basics/01_variables.js
@@ -3,6 +3,6 @@ let account_Emailid = "aqdasali9999@gmail.com";
 var account_Password = 12345678;
 account_City = "Islamabad";
 //Account Id doesn't changed because of constant variable
-//account_id = 121;
-console.table([account_id, account_Emailid, account_Password, account_City]);
-// console.log(account_id);
+account_id = 121;
+// console.table([account_id, account_Emailid, account_Password, account_City]);
+console.log(account_id);
