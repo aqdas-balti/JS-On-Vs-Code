d9acffc213359150eb260936142bc4cb4ce2526f
Aqdas Ali <aqdasali584@gmail.com>
2024-06-03 13:23:05 +0500

    Array Basics

---
+++ 02-Basics/01_Array.js
@@ -0,0 +1,10 @@
+//Arrays
+let myArray1 = [1, 2, 3, 4, 5];
+let myArray2 = ["Aqdas", "Anwar", "Furqan", "Irfan"];
+let myArray3 = new Array(1, 2, 3, 4, 5);
+console.log(myArray1); //[ 1, 2, 3, 4, 5 ]
+console.log(myArray1[0]); //1
+console.log(myArray2); //[ 'Aqdas', 'Anwar', 'Furqan', 'Irfan' ]
+console.log(myArray2[2]); //Furqan
+console.log(myArray3); //[ 1, 2, 3, 4, 5 ]
+console.log(myArray3[4]); //5
