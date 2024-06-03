dfe0990288e418fa690c85218098ead99f9e4746
Aqdas Ali <aqdasali584@gmail.com>
2024-06-03 15:04:02 +0500

    Some Methods of Array

--- 02-Basics/01_Array.js
+++ 02-Basics/01_Array.js
@@ -28,3 +28,22 @@ console.log(myArray2); //[ 'Aqdas', 'Anwar', 'Furqan', 'Irfan', 'Zafran' ]
 myArray3.push(6);
 myArray3.pop(); //[ 1, 2, 3, 4, 5]
 console.log(myArray3); //[ 1, 2, 3, 4, 5, 6 ]
+
+//Shift and un Shift Methods of Array
+let myArray4 = [1, 2, 3, 4, 5];
+myArray1.unshift(0); //unshift is used to add a value to the beginning point of an array
+console.log(myArray4); //[ 0, 1, 2, 3, 4, 5 ]
+myArray1.shift(); //Shift is used to remove the beginning value of an array
+console.log(myArray4); //[ 1, 2, 3, 4, 5 ]
+
+//Some Questioners Method in arrays are:
+console.log(myArray4.includes(10)); //false //It declares boolean ans t/f
+console.log(myArray4.indexOf(9)); //-1 declare if the value is not existing in array
+console.log(myArray4); //[1, 2, 3, 4, 5];
+console.log(myArray4.indexOf(4)); // (3)   index no 3 have value 4 is present
+
+//join is used to convert the array into a string
+let myArray5 = [1, 2, 3, 4, 5];
+let newArray1 = myArray5.join();
+console.log(newArray1); //1,2,3,4,5
+console.log(typeof newArray1); //string
